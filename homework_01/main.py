ab3503a0285d34b738632ed8cd5a62c0a3e03ad9
Igor <stormilk38@gmail.com>
2022-03-30 12:07:37 +0300

    Delete all comments

--- homework_01/main.py
+++ homework_01/main.py
@@ -3,21 +3,13 @@
 Функции и структуры данных
 """
 
-
-def power_numbers(*numbers):
-    """
-    функция, которая принимает N целых чисел,
-    и возвращает список квадратов этих чисел
-    >>> power_numbers(1, 2, 5, 7)
-    <<< [1, 4, 25, 49]
-    """
-    return [number ** 2 for number in numbers]
-
-
-# filter types
 ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
+
+
+def power_numbers(*numbers):
+    return [number ** 2 for number in numbers]
 
 
 def is_odd(n):
@@ -40,32 +32,9 @@ def is_prime(n):
 
 
 def filter_numbers(numbers_list, filter_type):
-    """
-    функция, которая на вход принимает список из целых чисел,
-    и возвращает только чётные/нечётные/простые числа
-    (выбор производится передачей дополнительного аргумента)
-
-    >>> filter_numbers([1, 2, 3], ODD)
-    <<< [1, 3]
-    >>> filter_numbers([2, 3, 4, 5], EVEN)
-    <<< [2, 4]
-    """
     if filter_type == ODD:
         return list(filter(is_odd, numbers_list))
     if filter_type == EVEN:
         return list(filter(is_even, numbers_list))
     if filter_type == PRIME:
         return list(filter(is_prime, numbers_list))
-
-if __name__ == "__main__":
-    # pow_numbers = power_numbers(1, 7, 19, -15, 25, 2, -1)
-    # print(pow_numbers)
-    #
-    # odd_numbers = filter_numbers([1, 2, 100, 25, 199], ODD)
-    # print(odd_numbers)
-    #
-    # even_numbers = filter_numbers([1, 2, 100, 25, 199], EVEN)
-    # print(even_numbers)
-
-    prime_numbers = filter_numbers([0,1,2,3,4,5,6,7,8,9,10,11], PRIME)
-    print(prime_numbers)
